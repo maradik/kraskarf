835c843d12d53b801fc952ab171257338deb6f26
Maradik <maradik@bk.ru>
2015-01-28 21:59:36 +0500

    first commit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore.an/admin/');
+define('HTTP_CATALOG', 'http://ocstore.an/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore.an/admin/');
+define('HTTPS_CATALOG', 'http://ocstore.an/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ocstore.an/admin/');
+define('DIR_SYSTEM', '/var/www/ocstore.an/system/');
+define('DIR_DATABASE', '/var/www/ocstore.an/system/database/');
+define('DIR_LANGUAGE', '/var/www/ocstore.an/admin/language/');
+define('DIR_TEMPLATE', '/var/www/ocstore.an/admin/view/template/');
+define('DIR_CONFIG', '/var/www/ocstore.an/system/config/');
+define('DIR_IMAGE', '/var/www/ocstore.an/image/');
+define('DIR_CACHE', '/var/www/ocstore.an/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ocstore.an/download/');
+define('DIR_LOGS', '/var/www/ocstore.an/system/logs/');
+define('DIR_CATALOG', '/var/www/ocstore.an/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'antony26_kraskarf');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ocstore.an/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ocstore.an/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/ocstore.an/catalog/');
+define('DIR_SYSTEM', '/var/www/ocstore.an/system/');
+define('DIR_DATABASE', '/var/www/ocstore.an/system/database/');
+define('DIR_LANGUAGE', '/var/www/ocstore.an/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/ocstore.an/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/ocstore.an/system/config/');
+define('DIR_IMAGE', '/var/www/ocstore.an/image/');
+define('DIR_CACHE', '/var/www/ocstore.an/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/ocstore.an/download/');
+define('DIR_LOGS', '/var/www/ocstore.an/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'antony26_kraskarf');
+define('DB_PREFIX', '');
+?>
